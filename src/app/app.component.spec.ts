b22bcf23fefb3640f43e23ff0267be3658bb62de
cassidyantic <62527185+cassidyantic@users.noreply.github.com>
2020-08-21 15:23:50 -0400

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'learning'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learning');
+    expect(app.name).toEqual('learning');
   });
 
   it('should render title', () => {
